185415d5c8464d44ab7fe3ffabda897a9260108f
movingChurch <seong9115@gmail.com>
2025-03-25 17:57:44 +0900

    feat/Added a function for test

---
+++ libraries/math/include/math/add.hpp
@@ -0,0 +1,10 @@
+#ifndef ULTIMATE_CMAKE_TEMPLATE__MATH__ADD_HPP_
+#define ULTIMATE_CMAKE_TEMPLATE__MATH__ADD_HPP_
+
+#include <cstdint>
+
+namespace ultimate_cmake_template::math {
+auto Add(int32_t lhs, int32_t rhs) -> int32_t;
+}  // namespace ultimate_cmake_template::math
+
+#endif  // ULTIMATE_CMAKE_TEMPLATE_MATH_ADD_HPP_
---
+++ libraries/math/src/add.cpp
@@ -0,0 +1,7 @@
+#include "math/add.hpp"
+
+#include <cstdint>
+
+namespace ultimate_cmake_template::math {
+auto Add(int32_t lhs, int32_t rhs) -> int32_t { return lhs + rhs; }
+}  // namespace ultimate_cmake_template::math
