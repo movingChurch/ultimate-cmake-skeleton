157f84c74022fbd6414f2bb54f32d587a55444c6
movingChurch <seong9115@gmail.com>
2025-03-25 16:15:58 +0900

    feat/Changed folder name

---
+++ applications/printing_goodbye_world/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+namespace ultimate_cmake_template {
+namespace applications {
+namespace printing_goodbye_world {
+auto main() -> int32_t {
+  std::cout << "Goodbye, World!" << std::endl;
+  return 0;
+}
+} // namespace printing_goodbye_world
+} // namespace applications
+} // namespace ultimate_cmake_template
---
+++ applications/printing_hello_world/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+namespace ultimate_cmake_template {
+namespace applications {
+namespace printing_hello_world {
+auto main() -> int32_t {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
+} // namespace printing_hello_world
+} // namespace applications
+} // namespace ultimate_cmake_template
