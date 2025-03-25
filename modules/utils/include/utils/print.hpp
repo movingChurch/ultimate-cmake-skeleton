98fe1a6523258143978b1eaaaac47788fea85474
movingChurch <seong9115@gmail.com>
2025-03-25 16:19:00 +0900

    feat/Added print function

---
+++ modules/utils/include/utils/print.hpp
@@ -0,0 +1,12 @@
+#ifndef ULTIMATE_CMAKE_TEMPLATE__UTILS__PRINT_HPP
+#define ULTIMATE_CMAKE_TEMPLATE__UTILS__PRINT_HPP
+
+#include <iostream>
+
+namespace ultimate_cmake_template {
+namespace utils {
+void print(const std::string &message);
+} // namespace utils
+} // namespace ultimate_cmake_template
+
+#endif
---
+++ modules/utils/src/print.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "utils/print.hpp"
+
+namespace ultimate_cmake_template {
+namespace utils {
+void print(const std::string &message) { std::cout << message << std::endl; }
+} // namespace utils
+} // namespace ultimate_cmake_template
